76f1700f62de80793411b2a9727e6e51247fde84
Niklas Koep <niklas.koep@gmail.com>
2020-04-01 11:07:44 +0200

    Support both email forwarding and reply relaying
    Signed-off-by: Niklas Koep <niklas.koep@gmail.com>

--- src/lampions_lambda_function.py
+++ src/lampions_lambda_function.py
@@ -17,16 +17,16 @@ def _retrieve_message(message_id):
     return message["Body"].read()
 
 
-def _determine_forward_addresses(recipients):
+def _determine_forward_address(recipients):
+    region = os.environ["LAMPIONS_REGION"]
     domain = os.environ["LAMPIONS_DOMAIN"]
     bucket = f"lampions.{domain}"
 
-    s3 = boto3.client("s3")
+    s3 = boto3.client("s3", region_name=region)
     try:
         response = s3.get_object(Bucket=bucket, Key="routes.json")
     except s3.exceptions.NoSuchKey:
         return None
-
     data = response["Body"].read()
     try:
         dictionary = json.loads(data)
@@ -46,10 +46,21 @@ def _determine_forward_addresses(recipients):
                     print(f"Not forwarding email to '{recipient}' "
                           "(route inactive)")
                 else:
-                    forward_addresses.append(
-                        email.utils.formataddr((name, route["forward"])))
+                    if name and name != address:
+                        forward_address = email.utils.formataddr(
+                            (name, route["forward"]))
+                    else:
+                        forward_address = route["forward"]
+                    forward_addresses.append((alias, forward_address))
                     break
-    return forward_addresses
+
+    if not forward_addresses:
+        raise SystemExit(f"No valid alias found for '{recipients}'")
+    forward_address, *_ = forward_addresses
+    if len(forward_addresses) > 1:
+        print("Multiple forward addresses found! Only forwarding to "
+              f"'{forward_address}'.")
+    return forward_address
 
 
 def _get_verified_addresses():
@@ -66,41 +77,134 @@ def _compute_sha224_hash(string):
     return hash.hexdigest()
 
 
+def _get_recipient_relations():
+    region = os.environ["LAMPIONS_REGION"]
+    domain = os.environ["LAMPIONS_DOMAIN"]
+    bucket = f"lampions.{domain}"
+    s3 = boto3.client("s3", region_name=region)
+    try:
+        response = s3.get_object(Bucket=bucket, Key="recipients.json")
+    except s3.exceptions.NoSuchKey:
+        return {}
+    data = response["Body"].read()
+    try:
+        dictionary = json.loads(data)
+    except json.decoder.JSONDecodeError:
+        return {}
+    return dictionary["recipients"]
+
+
+def _dict_to_formatted_json(dictionary):
+    return json.dumps(dictionary, indent=2)
+
+
+def _set_recipient_relations(recipients):
+    region = os.environ["LAMPIONS_REGION"]
+    domain = os.environ["LAMPIONS_DOMAIN"]
+    bucket = f"lampions.{domain}"
+
+    recipients_string = _dict_to_formatted_json({"recipients": recipients})
+    s3 = boto3.client("s3", region_name=region)
+    s3.put_object(Bucket=bucket, Key="recipients.json",
+                   Body=recipients_string)
+
+
+def _get_recipient_by_hash(alias, address_hash):
+    recipients = _get_recipient_relations()
+    recipients_for_alias = recipients.get(alias)
+    if recipients_for_alias is not None:
+        return recipients_for_alias.get(address_hash)
+    return None
+
+
+def _add_recipient_relation(alias, address, reply_to):
+    address_hash = _compute_sha224_hash(address)
+    recipients = _get_recipient_relations()
+    recipients_for_alias = recipients.get(alias)
+    if recipients_for_alias is None:
+        recipients[alias] = {address_hash: reply_to}
+    else:
+        recipients_for_alias[address_hash] = reply_to
+    _set_recipient_relations(recipients)
+    domain = os.environ["LAMPIONS_DOMAIN"]
+    return f"{alias}+{address_hash}@{domain}"
+
+
+def _determine_reply_recipient(recipients):
+    if len(recipients) > 1:
+        return None
+    domain = os.environ["LAMPIONS_DOMAIN"]
+    recipient, = recipients
+    name, address = email.utils.parseaddr(recipient)
+    if "+" in address and address.endswith(domain):
+        return recipient
+    return None
+
+
 def _send_message(message_id):
     file = _retrieve_message(message_id).decode("utf8")
     mail = email.message_from_string(file)
 
-    recipients = mail.get_all("To")
-    forward_addresses = _determine_forward_addresses(recipients)
-    if not forward_addresses:
-        print(f"No alias found for '{recipients}'")
-        return
-
-    domain = os.environ["LAMPIONS_DOMAIN"]
     original_sender = mail["From"]
-    if mail["Reply-To"] is None:
-        mail.add_header("Reply-To", original_sender)
-    original_name, address = email.utils.parseaddr(original_sender)
-    if original_name:
-        name = f"{original_name} (via) {address}"
+    reply_to = mail["Reply-To"]
+    if reply_to is None:
+        reply_to = original_sender
+
+    unwanted_headers = [
+        # Return-Path addresses must be verified in SES, which we only have
+        # control over if we're sending reply emails.
+        "Return-Path",
+        # Preexisting DKIM signature headers might trigger
+        # 'InvalidParameterValue' errors in the 'SendRawEmail' endpoint.
+        "DKIM-Signature",
+        # We don't need to distinguish between 'From' and 'Sender' headers.
+        "Sender",
+        # No matter whether we're forwarding or sendign a reply email, we
+        # always want to use the 'From' header as 'Reply-To' header, so just
+        # remove the latter.
+        "Reply-To",
+        # When sending reply emails, these two headers leak the original sender
+        # address.
+        "Received-SPF",
+        "Authentication-Results"
+    ]
+    for header in unwanted_headers:
+        del mail[header]
+
+    origin_name, origin_address = email.utils.parseaddr(reply_to)
+    verified_addresses = _get_verified_addresses()
+
+    # Email is a reply from one of our verified addresses to the original
+    # sender.
+    recipients = mail.get_all("To")
+    reply_recipient = _determine_reply_recipient(recipients)
+    if origin_address in verified_addresses and reply_recipient is not None:
+        _, address = email.utils.parseaddr(reply_recipient)
+        alias, address_hash = address.split("@")[0].split("+")
+
+        domain = os.environ["LAMPIONS_DOMAIN"]
+        sender = email.utils.formataddr((origin_name, f"{alias}@{domain}"))
+        mail.replace_header("From", sender)
+
+        recipient = _get_recipient_by_hash(alias, address_hash)
+        mail.replace_header("To", recipient)
+        destinations = [recipient]
     else:
-        name = address
-    address_hash = _compute_sha224_hash(address)
-    sender = email.utils.formataddr((name, f"lampions@{domain}"))
-    mail.replace_header("From", sender)
-
-    # Return-Path addresses must be verified in SES, which we have no control
-    # over. Drop the header instead.
-    del mail["Return-Path"]
-
-    # Remove any preexisting DKIM signature headers as this tends to trigger
-    # 'InvalidParameterValue' errors in the 'SendRawEmail' endpoint.
-    del mail["DKIM-Signature"]
+        if origin_name:
+            name = f"{origin_name} (via) {origin_address}"
+        else:
+            name = origin_address
+        alias, forward_address = _determine_forward_address(mail.get_all("To"))
+        sender_address = _add_recipient_relation(
+            alias, origin_address, reply_to)
+        sender = email.utils.formataddr((name, sender_address))
+        mail.replace_header("From", sender)
+        destinations = [forward_address]
 
     region = os.environ["LAMPIONS_REGION"]
     kwargs = {
         "Source": sender,
-        "Destinations": forward_addresses,
+        "Destinations": destinations,
         "RawMessage": {
             "Data": mail.as_string()
         }
