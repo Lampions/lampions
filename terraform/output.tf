4bfd88ee808e951b8cf773524eb33d311efb8732
Niklas Koep <niklas.koep@gmail.com>
2021-09-24 23:24:46 +0200

    Add output definitions
    Signed-off-by: Niklas Koep <niklas.koep@gmail.com>

--- terraform/output.tf
+++ terraform/output.tf
@@ -1,8 +1,20 @@
-output "access_key" {
+output "Region" {
+  value = var.region
+}
+
+output "Domain" {
+  value = var.domain
+}
+
+output "AccessKeyId" {
   value = aws_iam_access_key.lampions_iam_route_user_access_key.id
 }
 
-output "secret_access_key" {
+output "SecretAccessKey" {
   value = aws_iam_access_key.lampions_iam_route_user_access_key.secret
   sensitive = true
+}
+
+output "DkimTokens" {
+  value = aws_ses_domain_dkim.lampions_ses_domain_dkim.dkim_tokens
 }
