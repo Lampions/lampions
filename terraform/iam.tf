bc057e7ed81110298ed4d88b1a1192a65cb8339c
Niklas Koep <niklas.koep@gmail.com>
2021-09-24 22:23:43 +0200

    Create route user
    Signed-off-by: Niklas Koep <niklas.koep@gmail.com>

---
+++ terraform/iam.tf
@@ -0,0 +1,35 @@
+# Route user.
+resource "aws_iam_user" "lampions_iam_route_user" {
+  name = "${local.lampions_prefix}RouteUser"
+}
+
+# Route user policy document.
+data "aws_iam_policy_document" "lampions_iam_route_user_policy_document" {
+  statement {
+    sid       = "${local.lampions_prefix}S3ListBucket"
+    effect    = "Allow"
+    actions   = ["s3:ListBucket"]
+    resources = ["${aws_s3_bucket.lampions_s3_bucket.arn}"]
+  }
+
+  statement {
+    sid     = "${local.lampions_prefix}S3GetPutRoutes"
+    effect  = "Allow"
+    actions = ["s3:GetObject", "s3:PutObject"]
+    resources = [
+      "${aws_s3_bucket.lampions_s3_bucket.arn}/routes.json",
+      "${aws_s3_bucket.lampions_s3_bucket.arn}/recipients.json"
+    ]
+  }
+}
+
+# Route user policy.
+resource "aws_iam_user_policy" "lampions_iam_route_user_policy" {
+  user   = aws_iam_user.lampions_iam_route_user.name
+  policy = data.aws_iam_policy_document.lampions_iam_route_user_policy_document.json
+}
+
+# Access key.
+resource "aws_iam_access_key" "lampions_iam_route_user_access_key" {
+  user   = aws_iam_user.lampions_iam_route_user.name
+}
--- terraform/main.tf
+++ terraform/main.tf
@@ -37,36 +37,3 @@ provider "aws" {
 }
 
 data "aws_caller_identity" "current" {}
-
-# S3 bucket for incoming emails and route aliases.
-resource "aws_s3_bucket" "lampions_s3_bucket" {
-  bucket = "lampions.${var.domain}"
-
-  versioning {
-    enabled = true
-  }
-}
-
-# Bucket policy.
-data "aws_iam_policy_document" "lampions_s3_bucket_policy_document" {
-  statement {
-    sid    = "${local.lampions_prefix}SesS3Put"
-    effect = "Allow"
-    principals {
-      type        = "Service"
-      identifiers = ["ses.amazonaws.com"]
-    }
-    actions   = ["s3:PutObject"]
-    resources = ["${aws_s3_bucket.lampions_s3_bucket.arn}/inbox/*"]
-    condition {
-      test     = "StringEquals"
-      variable = "aws:Referer"
-      values   = [data.aws_caller_identity.current.account_id]
-    }
-  }
-}
-
-resource "aws_s3_bucket_policy" "lampions_s3_bucket_policy" {
-  bucket = aws_s3_bucket.lampions_s3_bucket.id
-  policy = data.aws_iam_policy_document.lampions_s3_bucket_policy_document.json
-}
