12c8d460ae9315f0885d7a15be52e052b9319ae9
Niklas Koep <niklas.koep@gmail.com>
2021-09-24 08:30:02 +0200

    Move terraform files to separate directory
    Signed-off-by: Niklas Koep <niklas.koep@gmail.com>

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,3 +1,15 @@
+# Terraform configuration.
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 3.27"
+    }
+  }
+
+  required_version = ">= 0.14.9"
+}
+
 # External variable definitions.
 variable "region" {
   type        = string
@@ -17,23 +29,6 @@ variable "domain" {
 # Local variables.
 locals {
   lampions_prefix = join("", [for part in split(".", var.domain) : title(part)])
-}
-
-# Terraform configuration.
-terraform {
-  required_providers {
-    aws = {
-      source  = "hashicorp/aws"
-      version = "~> 3.27"
-    }
-  }
-
-  required_version = ">= 0.14.9"
-
-  # backend "s3" {
-  #   bucket = "lampions.${var}"
-  #   key    = "path/to/my/key"
-  # }
 }
 
 provider "aws" {
